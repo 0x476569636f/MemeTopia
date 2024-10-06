2df2b18799f6cb89b870c4c50401484cc5257349
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-07 02:28:09 +0700

    feat: add sign-in and sign-up screens

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+import React from 'react';
+import ScreenWrapper from '~/components/ScreenWrapperWithNavbar';
+
+const SignIn = () => {
+  return (
+    <ScreenWrapper>
+      <Text>SignIn</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default SignIn;
