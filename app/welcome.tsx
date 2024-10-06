9229f54147ee46d39ec33e69ba12488b61a3b6fd
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-07 02:27:59 +0700

    chore: finishing welcome screen

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, Pressable } from 'react-native';
 import { Button } from '~/components/nativewindui/Button';
 import ScreenWrapper from '~/components/ScreenWrapperWithNavbar';
 import { Text } from '~/components/nativewindui/Text';
+import { useRouter } from 'expo-router';
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <ScreenWrapper>
       <View className="flex-1 items-center justify-around px-4">
@@ -15,14 +17,22 @@ const Welcome = () => {
           <Text className="text-center" variant={'largeTitle'}>
             MemeTopia
           </Text>
-          <Text className="text-center" variant={'caption1'}>
+          <Text className="text-center" variant={'footnote'}>
             Tertawa adalah hak semua orang. MemeTopia adalah tempatnya.
           </Text>
         </View>
         <View className="w-full gap-7">
-          <Button>
-            <Text>Login</Text>
+          <Button onPress={() => router.push('/sign-up')}>
+            <Text>Mulai Sekarang</Text>
           </Button>
+        </View>
+        <View className="mb-6 flex flex-row items-center gap-1">
+          <Text variant={'footnote'}>Sudah punya akun?</Text>
+          <Pressable onPress={() => router.push('/sign-in')}>
+            <Text variant={'footnote'} className="font-semibold text-primary">
+              Masuk
+            </Text>
+          </Pressable>
         </View>
       </View>
     </ScreenWrapper>
