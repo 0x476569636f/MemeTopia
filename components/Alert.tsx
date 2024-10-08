2e72557e47deb1d24f4b8a5134a0a1de18ef1600
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-08 17:31:11 +0700

    feat: add Alert component

---
+++ components/Alert.tsx
@@ -0,0 +1,30 @@
+import { Modal, View, TouchableOpacity } from 'react-native';
+import * as React from 'react';
+import { Text } from './nativewindui/Text';
+
+type AlertProps = {
+  visible: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+};
+
+const Alert: React.FC<AlertProps> = ({ visible, onClose, title, message }) => {
+  return (
+    <Modal transparent={true} visible={visible} animationType="fade" onRequestClose={onClose}>
+      <View className="flex-1 items-center justify-center">
+        <View className="mx-4 w-full max-w-md rounded-lg bg-card p-6">
+          <Text variant="title2" className="mb-4">
+            {title}
+          </Text>
+          <Text className="mb-6">{message}</Text>
+          <TouchableOpacity className="rounded-md bg-blue-500 p-2" onPress={onClose}>
+            <Text className="text-center text-white">Tutup</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default Alert;
