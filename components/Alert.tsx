dd505f79062eb9b2b99eae251b9610716c335449
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-08 19:27:35 +0700

    feat: add actionText prop to Alert component

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -7,19 +7,26 @@ type AlertProps = {
   onClose: () => void;
   title: string;
   message: string;
+  actionText?: string;
 };
 
-const Alert: React.FC<AlertProps> = ({ visible, onClose, title, message }) => {
+const Alert: React.FC<AlertProps> = ({
+  visible,
+  onClose,
+  title,
+  message,
+  actionText = 'Tutup',
+}) => {
   return (
     <Modal transparent={true} visible={visible} animationType="fade" onRequestClose={onClose}>
       <View className="flex-1 items-center justify-center">
-        <View className="mx-4 w-full max-w-md rounded-lg bg-card p-6">
+        <View className="mx-4 w-11/12 max-w-md rounded-lg bg-card p-6">
           <Text variant="title2" className="mb-4">
             {title}
           </Text>
           <Text className="mb-6">{message}</Text>
           <TouchableOpacity className="rounded-md bg-blue-500 p-2" onPress={onClose}>
-            <Text className="text-center text-white">Tutup</Text>
+            <Text className="text-center text-white">{actionText}</Text>
           </TouchableOpacity>
         </View>
       </View>
