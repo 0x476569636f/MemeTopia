83c09320b5442b87ed2efe82a6630282b7f7ee6e
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-08 19:31:31 +0700

    feat: sign up functionality

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 import React from 'react';
 import ScreenWrapper from '~/components/ScreenWrapperWithNavbar';
 import { BackButton } from '~/components/BackButton';
@@ -12,6 +12,9 @@ import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Button } from '~/components/nativewindui/Button';
 import Loading from '~/components/Loading';
+import { supabase } from '~/lib/supabase';
+import Alert from '~/components/Alert';
+import { type AuthError } from '@supabase/supabase-js';
 
 const schema = yup.object().shape({
   name: yup.string().required('* Nama harus di isi').min(3, 'Nama minimal 3 karakter'),
@@ -24,6 +27,8 @@ const SignUp = () => {
   const { isDarkColorScheme } = useColorScheme();
   const router = useRouter();
   const [loading, setLoading] = React.useState(false);
+  const [errorState, setErrorState] = React.useState(false);
+  const [errorObj, setErrorObj] = React.useState<AuthError | null>(null);
 
   const {
     control,
@@ -41,137 +46,188 @@ const SignUp = () => {
 
   type FormData = yup.InferType<typeof schema>;
 
-  const onPressSend = (formData: FormData) => {
-    console.log('formData', formData.email);
+  const onPressSend = async (formData: FormData) => {
+    const name = formData.name.trim();
+    const email = formData.email.trim();
+    const password = formData.password.trim();
+
+    setLoading(true);
+
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          name,
+        },
+      },
+    });
+
+    setLoading(false);
+    if (error) {
+      setErrorState(true);
+      setErrorObj(error);
+      return;
+    }
   };
 
+  if (errorState) {
+    return (
+      <Alert
+        visible={errorState}
+        onClose={
+          errorObj?.status === 422
+            ? () => {
+                setErrorState(false);
+                setErrorObj(null);
+                router.replace('/sign-in');
+              }
+            : () => setErrorState(false)
+        }
+        title="Gagal Mendaftar"
+        message={errorObj?.message || 'Terjadi kesalahan saat mendaftar'}
+        actionText={errorObj?.status === 422 ? 'Masuk' : 'Tutup'}
+      />
+    );
+  }
+
   return (
-    <ScreenWrapper>
-      <View className="flex flex-1 gap-11 px-3">
-        <BackButton onPress={() => router.back()} />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScreenWrapper>
+        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', padding: 16 }}>
+          <View className="flex flex-1 gap-11 px-3">
+            <BackButton onPress={() => router.back()} />
 
-        <View className="gap-y-1">
-          <Text className="text-3xl font-bold">MemeTopia Menantimu!</Text>
-          <Text className="text-lg font-normal tracking-wide">
-            Bergabunglah dan Bagikan Tawa Bersama Kami!
-          </Text>
-        </View>
-        <View className="gap-8 rounded-xl border border-border bg-card p-4 pb-6">
-          <View className="mt-4 gap-2">
-            <Controller
-              control={control}
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  icon={
-                    <AntDesign
-                      name="user"
-                      size={24}
-                      color={isDarkColorScheme ? 'white' : 'black'}
-                    />
-                  }
-                  placeholder="Masukan Nama Anda"
-                  value={value}
-                  onChangeText={onChange}
-                />
-              )}
-              name="name"
-            />
-            {errors.name && (
-              <Text variant={'footnote'} className="text-red-500">
-                {errors.name.message}
+            <View className="gap-y-1">
+              <Text className="text-3xl font-bold">MemeTopia Menantimu!</Text>
+              <Text className="text-lg font-normal tracking-wide">
+                Bergabunglah dan Bagikan Tawa Bersama Kami!
               </Text>
-            )}
-            <Controller
-              control={control}
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  icon={
-                    <AntDesign
-                      name="mail"
-                      size={24}
-                      color={isDarkColorScheme ? 'white' : 'black'}
+            </View>
+
+            <View className="gap-8 rounded-xl border border-border bg-card p-4 pb-6">
+              <View className="mt-4 gap-2">
+                <Controller
+                  control={control}
+                  rules={{
+                    required: true,
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      icon={
+                        <AntDesign
+                          name="user"
+                          size={24}
+                          color={isDarkColorScheme ? 'white' : 'black'}
+                        />
+                      }
+                      placeholder="Masukan Nama Anda"
+                      value={value}
+                      onChangeText={onChange}
                     />
-                  }
-                  placeholder="Masukan Email Anda"
-                  value={value}
-                  onChangeText={onChange}
+                  )}
+                  name="name"
                 />
-              )}
-              name="email"
-            />
-            {errors.email && (
-              <Text variant={'footnote'} className="text-red-500">
-                {errors.email.message}
-              </Text>
-            )}
-            <Controller
-              control={control}
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  icon={
-                    <AntDesign
-                      name="lock"
-                      size={24}
-                      color={isDarkColorScheme ? 'white' : 'black'}
+                {errors.name && (
+                  <Text variant={'footnote'} className="text-red-500">
+                    {errors.name.message}
+                  </Text>
+                )}
+                <Controller
+                  control={control}
+                  rules={{
+                    required: true,
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      icon={
+                        <AntDesign
+                          name="mail"
+                          size={24}
+                          color={isDarkColorScheme ? 'white' : 'black'}
+                        />
+                      }
+                      placeholder="Masukan Email Anda"
+                      value={value}
+                      onChangeText={onChange}
                     />
-                  }
-                  placeholder="Masukan Password"
-                  value={value}
-                  onChangeText={onChange}
-                  secureTextEntry
+                  )}
+                  name="email"
                 />
-              )}
-              name="password"
-            />
-            {errors.password && (
-              <Text variant={'footnote'} className="text-red-500">
-                {errors.password.message}
-              </Text>
-            )}
-            <Controller
-              control={control}
-              rules={{
-                required: true,
-              }}
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  icon={
-                    <AntDesign
-                      name="lock"
-                      size={24}
-                      color={isDarkColorScheme ? 'white' : 'black'}
+                {errors.email && (
+                  <Text variant={'footnote'} className="text-red-500">
+                    {errors.email.message}
+                  </Text>
+                )}
+                <Controller
+                  control={control}
+                  rules={{
+                    required: true,
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      icon={
+                        <AntDesign
+                          name="lock"
+                          size={24}
+                          color={isDarkColorScheme ? 'white' : 'black'}
+                        />
+                      }
+                      placeholder="Masukan Password"
+                      value={value}
+                      onChangeText={onChange}
+                      secureTextEntry
                     />
-                  }
-                  placeholder="Masukan Konfirmasi Password"
-                  value={value}
-                  onChangeText={onChange}
-                  secureTextEntry
+                  )}
+                  name="password"
                 />
-              )}
-              name="confirmPassword"
-            />
-            {errors.confirmPassword && (
-              <Text variant={'footnote'} className="text-red-500">
-                {errors.confirmPassword.message}
-              </Text>
-            )}
+                {errors.password && (
+                  <Text variant={'footnote'} className="text-red-500">
+                    {errors.password.message}
+                  </Text>
+                )}
+                <Controller
+                  control={control}
+                  rules={{
+                    required: true,
+                  }}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      icon={
+                        <AntDesign
+                          name="lock"
+                          size={24}
+                          color={isDarkColorScheme ? 'white' : 'black'}
+                        />
+                      }
+                      placeholder="Masukan Konfirmasi Password"
+                      value={value}
+                      onChangeText={onChange}
+                      secureTextEntry
+                    />
+                  )}
+                  name="confirmPassword"
+                />
+                {errors.confirmPassword && (
+                  <Text variant={'footnote'} className="text-red-500">
+                    {errors.confirmPassword.message}
+                  </Text>
+                )}
+              </View>
+
+              <Button onPress={handleSubmit(onPressSend)} disabled={loading}>
+                {loading ? <Loading /> : <Text>Daftar Sekarang</Text>}
+              </Button>
+            </View>
           </View>
-
-          <Button onPress={handleSubmit(onPressSend)} disabled={loading}>
-            {loading ? <Loading /> : <Text>Daftar Sekarang</Text>}
-          </Button>
-        </View>
-      </View>
-    </ScreenWrapper>
+        </ScrollView>
+      </ScreenWrapper>
+    </KeyboardAvoidingView>
   );
 };
 
