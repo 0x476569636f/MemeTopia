717269d1f77aaee2bc096c908d602f16becddf98
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-20 05:58:53 +0700

    chore: update Input component to support multiline input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, TextInput, useColorScheme } from 'react-native';
+import { cn } from '~/lib/cn';
 
 type inputProps = {
   containerStyle?: string;
@@ -8,6 +9,8 @@ type inputProps = {
   placeholder?: string;
   onChangeText?: (value: string) => void;
   secureTextEntry?: boolean;
+  multiline?: boolean;
+  numberOfLines?: number;
   value?: string;
 };
 
@@ -17,13 +20,17 @@ const Input: React.FC<inputProps> = (props) => {
 
   return (
     <View
-      className={`h-12 flex-row items-center gap-2 rounded-md border border-gray-400 px-4 ${props.containerStyle}`}>
+      className={cn(
+        `h-12 flex-row items-center gap-2 rounded-md border border-gray-400 px-4 ${props.containerStyle}`
+      )}>
       {props.icon && props.icon}
       <TextInput
-        className="flex-1 text-black dark:text-white"
+        style={{ flex: 1, color: isDarkColorScheme ? 'white' : 'black' }}
         placeholderTextColor={isDarkColorScheme ? 'white' : 'black'}
         ref={props.inputRef}
         value={props.value}
+        multiline={props.multiline}
+        numberOfLines={props.numberOfLines}
         {...props}
       />
     </View>
