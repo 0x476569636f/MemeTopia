c8dfcf2feb4a90697cf693ddc251c71db89eabbb
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-26 14:02:52 +0700

    refactor: remove aspect ratio constraint in NewPost screen image picker

--- app/(authenticated)/new-post.tsx
+++ app/(authenticated)/new-post.tsx
@@ -42,7 +42,6 @@ const NewPost = () => {
     let res = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
       quality: 1,
     });
 
