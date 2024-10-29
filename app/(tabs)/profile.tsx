62f3c577ec66ceb6815a958f34a02585b196dc56
Irvan Pramana Putra <darknet1255@gmail.com>
2024-10-29 21:45:06 +0700

    feat: update myprofile screen to display user's posts and add refresh functionality

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,45 @@
 import { Feather } from '@expo/vector-icons';
 import { Image } from 'expo-image';
 import { router } from 'expo-router';
-import React, { useState } from 'react';
-import { Alert, Dimensions, Modal, ScrollView, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Alert,
+  Dimensions,
+  FlatList,
+  Modal,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
 import Avatar from '~/components/Avatar';
+import Loading from '~/components/Loading';
 import { Button } from '~/components/nativewindui/Button';
 import { Text } from '~/components/nativewindui/Text';
+import PostCard from '~/components/PostCard';
 import ScreenWrapper from '~/components/ScreenWrapperWithNavbar';
 import { useAuth } from '~/context/auth';
-import { hp } from '~/lib/common';
+import { fetchPostById } from '~/functions/post';
+import { getSupabaseFileUrl } from '~/functions/storage';
+import { getUserData } from '~/functions/user';
+import { hp, wp } from '~/lib/common';
 import { supabase } from '~/lib/supabase';
+import { useColorScheme } from '~/lib/useColorScheme';
+
+let limit = 0;
 
 const Profile = () => {
   const { user, setAuth }: any = useAuth();
   const [showFullScreen, setShowFullScreen] = useState(false);
   const screenWidth = Dimensions.get('window').width;
   const screenHeight = Dimensions.get('window').height;
+  const [posts, setPosts] = useState<any[] | undefined>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const { isDarkColorScheme } = useColorScheme();
+
   const handleLogout = async () => {
     Alert.alert('Logout', 'Apakah kamu yakin ingin keluar?', [
       {
@@ -37,9 +61,45 @@ const Profile = () => {
     ]);
   };
 
-  return (
-    <ScreenWrapper routeName="Profil Saya">
-      <ScrollView contentContainerStyle={{ padding: 16 }}>
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    setHasMore(true);
+
+    try {
+      const res = await fetchPostById(undefined, user.id);
+      if (res.success) {
+        setPosts(res.data);
+      }
+    } catch (error) {
+      console.error('Refresh error:', error);
+      Alert.alert('Error', 'Failed to refresh posts');
+    } finally {
+      setRefreshing(false);
+    }
+  }, []);
+
+  const getPost = async () => {
+    if (!hasMore) return null;
+    limit = limit + 6;
+    let res = await fetchPostById(limit, user.id);
+
+    if (res.success) {
+      if (posts?.length == res?.data?.length) setHasMore(false);
+      setPosts(res.data);
+    }
+  };
+
+  const UserHeader = ({
+    user,
+    router,
+    handleLogout,
+  }: {
+    user: any;
+    router: any;
+    handleLogout: () => void;
+  }) => {
+    return (
+      <>
         <View className="rounded-3xl bg-card">
           <View className="flex items-center justify-center">
             <TouchableOpacity onPress={() => setShowFullScreen(true)}>
@@ -77,39 +137,84 @@ const Profile = () => {
             </Button>
           </View>
         </View>
-      </ScrollView>
-      <Modal
-        visible={showFullScreen}
-        transparent={true}
-        animationType="slide"
-        onRequestClose={() => setShowFullScreen(false)}>
-        <View style={{ flex: 1, backgroundColor: 'black' }}>
-          <TouchableOpacity
-            style={{
-              position: 'absolute',
-              top: 40,
-              right: 20,
-              zIndex: 1,
-              padding: 10,
-              borderRadius: 20,
-              backgroundColor: 'rgba(0,0,0,0.5)',
-            }}
-            onPress={() => setShowFullScreen(false)}>
-            <Feather name="x" size={30} color="white" />
-          </TouchableOpacity>
-          <Avatar
-            uri={user?.image}
-            style={{
-              width: screenWidth,
-              height: screenHeight,
-              flex: 1,
-              resizeMode: 'contain',
-            }}
+
+        <Modal
+          visible={showFullScreen}
+          transparent={true}
+          animationType="slide"
+          onRequestClose={() => setShowFullScreen(false)}>
+          <View style={{ flex: 1, backgroundColor: 'black' }}>
+            <TouchableOpacity
+              style={{
+                position: 'absolute',
+                top: 40,
+                right: 20,
+                zIndex: 1,
+                padding: 10,
+                borderRadius: 20,
+                backgroundColor: 'rgba(0,0,0,0.5)',
+              }}
+              onPress={() => setShowFullScreen(false)}>
+              <Feather name="x" size={30} color="white" />
+            </TouchableOpacity>
+            <ImageViewer
+              imageUrls={[{ url: getSupabaseFileUrl(user?.image)?.uri as string }] as any}
+              enableSwipeDown
+              onSwipeDown={() => setShowFullScreen(false)}
+              backgroundColor="rgba(0,0,0,0.9)"
+              renderIndicator={() => <></>}
+            />
+          </View>
+        </Modal>
+      </>
+    );
+  };
+
+  return (
+    <ScreenWrapper routeName="Profil Saya">
+      <FlatList
+        data={posts}
+        ListHeaderComponent={<UserHeader user={user} router={router} handleLogout={handleLogout} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listStyle}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <PostCard item={item} currentUser={user} router={router} />}
+        scrollEventThrottle={16}
+        ListFooterComponent={
+          hasMore ? (
+            <View style={{ marginVertical: posts?.length == 0 ? 200 : 30 }}>
+              <Loading />
+            </View>
+          ) : (
+            <View style={{ marginVertical: 30 }}>
+              <Text variant="body" className="text-center">
+                Tidak ada lagi post
+              </Text>
+            </View>
+          )
+        }
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={['rgb(0, 123, 254)']}
+            tintColor={isDarkColorScheme ? 'rgb(0, 123, 254)' : '#000000'}
           />
-        </View>
-      </Modal>
+        }
+        onEndReached={() => {
+          getPost();
+        }}
+        onEndReachedThreshold={0.5}
+      />
     </ScreenWrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4),
+  },
+});
 
 export default Profile;
--- functions/post.ts
+++ functions/post.ts
@@ -3,24 +3,25 @@ import { uploadFile } from './storage';
 
 export const createOrUpdatePost = async (post: any) => {
   try {
-    if (post.file && typeof post.file == 'object') {
-      let isImage = post?.file?.type == 'image';
+    if (post.file && typeof post.file === 'object') {
+      let isImage = post?.file?.type === 'image';
       let folderName = isImage ? 'postImages' : 'postVideos';
       let fileRes = await uploadFile(folderName, post?.file?.uri, isImage);
-      if (fileRes.success) {
-        post.file = fileRes.data;
-      } else {
+
+      if (!fileRes.success) {
         return fileRes;
       }
 
-      const { data, error } = await supabase.from('posts').upsert(post).select().single();
-
-      if (error) {
-        console.error(error);
-        return { success: false, msg: 'Error when creating or updating post' };
-      }
-      return { success: true, data: data };
+      post.file = fileRes.data;
     }
+    const { data, error } = await supabase.from('posts').upsert(post).select().single();
+
+    if (error) {
+      console.error(error);
+      return { success: false, msg: 'Error when creating or updating post' };
+    }
+
+    return { success: true, data: data };
   } catch (error) {
     console.error(error);
     return { success: false, msg: 'Error when creating or updating post' };
